c9a66402604781b30c403294ff2853a614c62a8f
Cade Ginder <cadeginder@gmail.com>
2026-07-02 14:27:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello from JavaScript!");
+let cpuTemp = 42;
+console.log(cpuTemp);
+let status ="online";
+console.log(status);
+let cpu = document.getElementById("cpu-temp");
+cpu.textContent= "CPU Temperature: 42 C";
