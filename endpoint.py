4780d4240edb7256d0fd8fcaffbfbabc29d242d5
Cade Ginder <cadeginder@gmail.com>
2026-07-09 09:41:33 -0500

    Added function to display data from endpoint. Fixed endpoint so it wasn't only listening locally (working through ssh generally so needed). Fixed some formatting consistency issues.

--- endpoint.py
+++ endpoint.py
@@ -1,14 +1,16 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/server")
+@app.route("/server_stats")
 def server():
     return jsonify({
-        "cpu": 45,
-        "gpu": 65,
-        "jellyfin_status": "Running"
+        "cpu": "45",
+        "gpu": "65",
+        "jellyfin_status": "online"
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000,debug=True)
--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function updateClock() {
     clock.textContent = "Time: "+ now.toLocaleTimeString();
 }
 setInterval(updateClock, 1000);
+
+async function updateServer() {
+    let response = await fetch("http://192.168.0.46:5000/server_stats");
+    let data = await response.json();
+    console.log(data);
+    let cpuTemp = document.getElementById("cpu-temp");
+    let gpuTemp = document.getElementById("gpu-temp");
+    let jellyfinStatus = document.getElementById("jellyfin_status");
+    cpuTemp.textContent = "CPU: " + data.cpu + "°C";
+    gpuTemp.textContent = "GPU: " + data.gpu + "°C";
+    jellyfinStatus.textContent = "Jellyfin: " + data.jellyfin_status;
+}
+setInterval(updateServer, 1000);
